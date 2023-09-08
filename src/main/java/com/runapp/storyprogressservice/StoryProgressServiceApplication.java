343a6cd2240f7cb51159e93789efb8fa5cf8798b
Denis Durbalov <den.dyrbalov25@gmail.com>
2023-09-08 19:17:02 +0200

    first commit

---
+++ src/main/java/com/runapp/storyprogressservice/StoryProgressServiceApplication.java
@@ -0,0 +1,13 @@
+package com.runapp.storyprogressservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryProgressServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryProgressServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/runapp/storyprogressservice/StoryProgressServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runapp.storyprogressservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryProgressServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
